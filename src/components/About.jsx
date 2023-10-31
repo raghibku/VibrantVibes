3fd40351d79b1e2309ef46a03581bfb42c4f42e0
raghib-shahriar <shahraghib.ece16@gmail.com>
2023-10-31 23:38:28 +0600

    Font and Marquee final fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,11 +5,11 @@ const About = () => {
 
         <div name="about" className=" mb-8">
             <div className="hero min-h-screen">
-                <div className="hero-content flex-col md:flex-row-reverse">
+                <div className="hero-content flex-col md:flex-row">
                     
                     <div className='md:w-[50%] '>
                         <h1 className="text-5xl text-primary font-bold">About Us</h1>
-                        <p className="py-6">Welcome to Vibrant Vibes, your premier partner in creating unforgettable music experiences. With a passion for music and a dedication to excellence, we've been arranging Concerts and Music Festivals that rock the world since 2016.
+                        <p className="py-6 text-xl">Welcome to Vibrant Vibes, your premier partner in creating unforgettable music experiences. With a passion for music and a dedication to excellence, we've been arranging Concerts and Music Festivals that rock the world since 2016.
                         <br />
                         Our mission is to bring music to life by curating immersive and unforgettable events that resonate with music lovers of all genres. We take pride in our attention to detail and our commitment to delivering seamless, world-class productions.
                         </p>
--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Media = () => {
     return (
-        <div className="flex items-center">
+        <div className="flex items-center mb-16">
             <div className="hidden h-[110px] bg-primary px-4 text-4xl font-bold text-white md:flex justify-center items-center ">Partners</div>
             <Marquee className="text-lg bg-neutral py-2">
                 <img src="/images/1.png" className="h-24 py-4 px-6 " alt="" />
