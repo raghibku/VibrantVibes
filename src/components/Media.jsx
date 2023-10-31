904c4170bbc23ae84934f63125e684dc86f24e8c
raghib-shahriar <shahraghib.ece16@gmail.com>
2023-10-31 23:18:06 +0600

    Marquee fix 1

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -6,13 +6,13 @@ const Media = () => {
         <div className="flex items-center">
             <div className="hidden h-[110px] bg-primary px-4 text-4xl font-bold text-white justify-center items-center ">Partners</div>
             <Marquee className="text-lg bg-neutral py-2">
-                <img src="/public/images/1.png" className="h-24 py-4 px-6 " alt="" />
+                {/* <img src="/public/images/1.png" className="h-24 py-4 px-6 " alt="" /> */}
                 <img src="/public/images/2.png" className="h-24 py-4 px-6" alt="" />
-                <img src="/public/images/3.png" className="h-24 py-4 px-6" alt="" />
-                <img src="/public/images/4.png" className="h-24 py-4 px-6" alt="" />
+                {/* <img src="/public/images/3.png" className="h-24 py-4 px-6" alt="" />
+                <img src="/public/images/4.png" className="h-24 py-4 px-6" alt="" /> */}
                 <img src="/public/images/5.png" className="h-24 py-4 px-6" alt="" />
                 <img src="/public/images/7.png" className="h-24 py-4 px-6" alt="" />
-                <img src="/public/images/6.png" className="h-24 py-4 px-6" alt="" />
+                {/* <img src="/public/images/6.png" className="h-24 py-4 px-6" alt="" /> */}
                 <img src="/public/images/8.png" className="h-24 py-4 px-6" alt="" />
             </Marquee>
         </div>
