34c4ecb4d6dcfb759fae2f8580e7200f925587bb
raghib-shahriar <shahraghib.ece16@gmail.com>
2023-10-31 22:59:42 +0600

    Marquee Hidden

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Media = () => {
     return (
         <div className="flex items-center">
-            <div className="hidden h-[110px] bg-primary px-4 text-4xl font-bold text-white md:flex justify-center items-center ">Partners</div>
+            <div className="hidden h-[110px] bg-primary px-4 text-4xl font-bold text-white justify-center items-center ">Partners</div>
             <Marquee className="text-lg bg-neutral py-2">
                 <img src="/public/images/1.png" className="h-24 py-4 px-6 " alt="" />
                 <img src="/public/images/2.png" className="h-24 py-4 px-6" alt="" />
